edba0dbf81f0761100cb870928ed98bfce19c7d5
1ncarnat10n <tsaiwilliam888@gmail.com>
2023-03-10 14:02:40 -0800

    added camera offset

--- level.py
+++ level.py
@@ -2,7 +2,6 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
-from debug import debug
 
 class Level:
     def __init__(self):
@@ -11,7 +10,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         # sprite group setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group() 
 
         self.create_map()
@@ -28,6 +27,25 @@ class Level:
 
     def run(self):
         # update and draw the game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+
+        # general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_weight = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_weight
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
--- player.py
+++ player.py
@@ -43,17 +43,17 @@ class Player(pygame.sprite.Sprite):
         if direction == 'horizontal':
             for sprite in self.obstacle_sprites:
                 if sprite.rect.colliderect(self.rect):
-                    if self.direction.x > 0: #moving right
+                    if self.direction.x > 0: # moving right
                         self.rect.right = sprite.rect.left
-                    if self.direction.x < 0: #moving left
+                    if self.direction.x < 0: # moving left
                         self.rect.left = sprite.rect.right
         
         if direction == 'vertical':
             for sprite in self.obstacle_sprites:
                 if sprite.rect.colliderect(self.rect):
-                    if self.direction.y > 0: #moving down
+                    if self.direction.y > 0: # moving down
                         self.rect.bottom = sprite.rect.top
-                    if self.direction.y < 0: #moving up
+                    if self.direction.y < 0: # moving up
                         self.rect.top = sprite.rect.bottom
 
     def update(self):
