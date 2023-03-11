d524f1a6b4488c396f9cb81fa11820de95bc666a
1ncarnat10n <tsaiwilliam888@gmail.com>
2023-03-11 15:18:58 -0800

    fixed sprite locations

--- support.py
+++ support.py
@@ -14,6 +14,7 @@ def import_folder(path):
     surface_list = []
 
     for _, __, img_files in walk(path):
+        img_files.sort()
         for image in img_files:
             full_path =  path + '/' + image
             image_surf = pygame.image.load(full_path).convert_alpha()
