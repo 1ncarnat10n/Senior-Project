d3cabfb935735ce5e1c51e7f03f0d4c6ffd69f57
1ncarnat10n <tsaiwilliam888@gmail.com>
2023-03-10 16:17:50 -0800

    added map and floor graphics

--- level.py
+++ level.py
@@ -2,6 +2,8 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
+from support import *
 
 class Level:
     def __init__(self):
@@ -16,14 +18,23 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
-                if col == 'p':
-                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
+        layouts = {
+                'boundary': import_csv_layout('/Users/basedatlas/Desktop/SeniorProject/Senior-Project/map/map_FloorBlocks.csv')
+        }
+
+        for style, layout in layouts.items():
+            for row_index, row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        if style == 'boundary':
+                            Tile((x, y), [self.obstacle_sprites], 'invisible')
+        #         if col == 'x':
+        #             Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+        #         if col == 'p':
+        #             self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
+        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
         # update and draw the game
@@ -40,11 +51,19 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_weight = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
+        # creating the floor
+        self.floor_surf = pygame.image.load('/Users/basedatlas/Desktop/SeniorProject/Senior-Project/graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0, 0))
+
     def custom_draw(self, player):
 
         # getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_weight
+
+        # drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
 
         # for sprite in self.sprites():
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
