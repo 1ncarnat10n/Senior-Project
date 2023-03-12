033b9ed092bd8097ab47c6ab931cf1b512c8eac5
1ncarnat10n <tsaiwilliam888@gmail.com>
2023-03-11 16:38:53 -0800

    added weapons and animations

--- settings.py
+++ settings.py
@@ -1,4 +1,13 @@
+# game setup
 WIDTH    = 1280
 HEIGHT   = 720
 FPS      = 60
 TILESIZE = 64
+
+# weapons
+weapon_data = {
+    'sword': {'cooldown': 100, 'damage': 15,'graphic':'/Users/basedatlas/Desktop/SeniorProject/Senior-Project/graphics/weapons/sword/full.png'},
+    'lance': {'cooldown': 400, 'damage': 30,'graphic':'/Users/basedatlas/Desktop/SeniorProject/Senior-Project/graphics/weapons/lance/full.png'},
+    'axe': {'cooldown': 300, 'damage': 20, 'graphic':'/Users/basedatlas/Desktop/SeniorProject/Senior-Project/graphics/weapons/axe/full.png'},
+    'rapier':{'cooldown': 50, 'damage': 8, 'graphic':'/Users/basedatlas/Desktop/SeniorProject/Senior-Project/graphics/weapons/rapier/full.png'},
+    'sai':{'cooldown': 80, 'damage': 10, 'graphic':'/Users/basedatlas/Desktop/SeniorProject/Senior-Project/graphics/weapons/sai/full.png'}}
