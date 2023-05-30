a2ec036e1aa7459e9afe41544461c457c1e9050c
1ncarnat10n <tsaiwilliam888@gmail.com>
2023-05-29 17:21:55 -0700

    added audio

--- settings.py
+++ settings.py
@@ -35,3 +35,10 @@ weapon_data = {
 magic_data = {
 	'flame': {'strength': 5, 'cost': 20, 'graphic': '/Users/basedatlas/Desktop/SeniorProject/Senior-Project/graphics/particles/flame/fire.png'},
 	'heal' : {'strength': 20, 'cost': 10, 'graphic': '/Users/basedatlas/Desktop/SeniorProject/Senior-Project/graphics/particles/heal/heal.png'}}
+
+# enemy
+monster_data = {
+	'squid': {'health': 100,'exp':100,'damage':20,'attack_type': 'slash', 'attack_sound':'/Users/basedatlas/Desktop/SeniorProject/Senior-Project/audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 80, 'notice_radius': 360},
+	'raccoon': {'health': 300,'exp':250,'damage':40,'attack_type': 'claw',  'attack_sound':'/Users/basedatlas/Desktop/SeniorProject/Senior-Project/audio/attack/claw.wav','speed': 2, 'resistance': 3, 'attack_radius': 120, 'notice_radius': 400},
+	'spirit': {'health': 100,'exp':110,'damage':8,'attack_type': 'thunder', 'attack_sound':'/Users/basedatlas/Desktop/SeniorProject/Senior-Project/audio/attack/fireball.wav', 'speed': 4, 'resistance': 3, 'attack_radius': 60, 'notice_radius': 350},
+	'bamboo': {'health': 70,'exp':120,'damage':6,'attack_type': 'leaf_attack', 'attack_sound':'/Users/basedatlas/Desktop/SeniorProject/Senior-Project/audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 50, 'notice_radius': 300}}
