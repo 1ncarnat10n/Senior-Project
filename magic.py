041e3def02f2fd7558caa9d5ff504ba3b0d5d5f8
1ncarnat10n <tsaiwilliam888@gmail.com>
2023-06-02 00:12:32 -0700

    final touches, added sound and visual fixes

--- magic.py
+++ magic.py
@@ -5,9 +5,14 @@ from random import randint
 class MagicPlayer:
     def __init__(self, animation_player):
         self.animation_player = animation_player
+        self.sounds = {
+            'heal': pygame.mixer.Sound('/Users/basedatlas/Desktop/SeniorProject/Senior-Project/audio/heal.wav'),
+            'flame':pygame.mixer.Sound('/Users/basedatlas/Desktop/SeniorProject/Senior-Project/audio/Fire.wav')
+        }
 
     def heal(self, player, strength, cost, groups):
         if player.energy >= cost:
+            self.sounds['heal'].play()
             player.health += strength
             player.energy -= cost
             if player.health >= player.stats['health']:
@@ -17,8 +22,9 @@ class MagicPlayer:
 
     def flame(self, player, cost, groups):
         if player.energy >= cost:
-            # player.energy -= cost
-
+            player.energy -= cost
+            self.sounds['flame'].play()
+            
             if player.status.split('_')[0] == 'right': direction = pygame.math.Vector2(1, 0)
             elif player.status.split('_')[0] == 'left': direction = pygame.math.Vector2(-1, 0)
             elif player.status.split('_')[0] == 'up': direction = pygame.math.Vector2(0, -1)
